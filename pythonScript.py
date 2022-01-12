4aa1361fb2942481aa4cb37c8690a6e647f1bcb0
Student <csheng2@ualberta.ca>
2022-01-11 23:31:30 -0500

    add code to print out source code

--- pythonScript.py
+++ pythonScript.py
@@ -1,6 +1,11 @@
 import requests
 
 print(requests.__version__)
+
 page = requests.get('http://google.com')
 print(page)
 
+python_script_url = 'https://raw.githubusercontent.com/csheng2/CMPUT404-lab1/master/pythonScript.py'
+get_script_request = requests.get(python_script_url)
+
+print(get_script_request.content)
